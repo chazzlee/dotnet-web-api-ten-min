497136db2e26efe8904a967c7f9b00f7615bfce0
chazzlee <l.chazz00@gmail.com>
2023-02-11 13:24:41 -0500

    add entityframeworkcore

---
+++ Data/DataContext.cs
@@ -0,0 +1,45 @@
+using Microsoft.EntityFrameworkCore;
+using TenMin.Models;
+
+namespace TenMin.Data;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions<DataContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Category> Categories { get; set; } = null!;
+    public DbSet<Country> Countries { get; set; } = null!;
+    public DbSet<Owner> Owners { get; set; } = null!;
+    public DbSet<Pokemon> Pokemon { get; set; } = null!;
+    public DbSet<PokemonOwner> PokemonOwners { get; set; } = null!;
+    public DbSet<PokemonCategory> PokemonCategories { get; set; } = null!;
+    public DbSet<Review> Reviews { get; set; } = null!;
+    public DbSet<Reviewer> Reviewers { get; set; } = null!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<PokemonCategory>()
+            .HasKey(pc => new { pc.PokemonId, pc.CategoryId });
+        modelBuilder.Entity<PokemonCategory>()
+            .HasOne(p => p.Pokemon)
+            .WithMany(pc => pc.PokemonCategories)
+            .HasForeignKey(c => c.PokemonId);
+        modelBuilder.Entity<PokemonCategory>()
+            .HasOne(p => p.Category)
+            .WithMany(pc => pc.PokemonCategories)
+            .HasForeignKey(c => c.CategoryId);
+
+        modelBuilder.Entity<PokemonOwner>()
+            .HasKey(po => new { po.PokemonId, po.OwnerId });
+        modelBuilder.Entity<PokemonOwner>()
+            .HasOne(p => p.Pokemon)
+            .WithMany(po => po.PokemonOwners)
+            .HasForeignKey(p => p.PokemonId);
+        modelBuilder.Entity<PokemonOwner>()
+            .HasOne(p => p.Owner)
+            .WithMany(po => po.PokemonOwners)
+            .HasForeignKey(o => o.OwnerId);
+    }
+}
